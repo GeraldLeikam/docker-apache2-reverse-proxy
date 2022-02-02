aa9d3e68cd749d0166ba8f29ef81a24f00602405
Gerald Leikam <gerald.leikam@aol.com>
2022-02-03 00:10:31 +0100

    commited by software

--- scripts/startup_reverse_proxy.sh
+++ scripts/startup_reverse_proxy.sh
@@ -7,7 +7,7 @@ then
 fi
 echo "ServerName ${SERVERNAME}" >> /etc/apache2/apache2.conf
 
-#bin/bash -c "/usr/sbin/apache2ctl"
+bin/bash -c "/usr/sbin/apache2ctl start"
 #certbot -n --apache --agree-tos -m gerald.leikam@aol.com -d sub1.apache-proxy.owncloud.works
-
+bin/bash -c "/usr/sbin/apache2ctl stop"
 bin/bash -c "/usr/sbin/apache2ctl -DFOREGROUND"
