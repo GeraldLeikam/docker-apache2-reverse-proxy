e2debfc0ba60ad113bf433afe1d559b684a9e1eb
Gerald Leikam <gerald.leikam@aol.com>
2022-02-02 23:14:17 +0100

    commited by software

--- scripts/startup_reverse_proxy.sh
+++ scripts/startup_reverse_proxy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ -z "$(ls -A /etc/apache2/)" ]
+then
+  cp -rv /apache_orig/* /etc/apache2
+fi
 echo "ServerName ${SERVERNAME}" >> /etc/apache2/apache2.conf
 bin/bash -c "/usr/sbin/apache2ctl -DFOREGROUND"
