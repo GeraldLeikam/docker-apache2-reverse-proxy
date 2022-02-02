98e863ba5ee9824952acf727a294c346497e686f
Gerald Leikam <gerald.leikam@aol.com>
2022-02-03 00:00:48 +0100

    commited by software

--- scripts/startup_reverse_proxy.sh
+++ scripts/startup_reverse_proxy.sh
@@ -8,4 +8,4 @@ fi
 #bin/bash -c "/usr/sbin/apache2ctl"
 #certbot -n --apache --agree-tos -m gerald.leikam@aol.com -d sub1.apache-proxy.owncloud.works
 
-bin/bash -c "/usr/sbin/apache2ctl -DFOREGROUD"
+bin/bash -c "/usr/sbin/apache2ctl -DFOREGROUND"
